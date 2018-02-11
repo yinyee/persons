72b60f99691a358b899a0a1eaf843ccbd385546b
yinyee <anon@anons-MacBook-Air.local>
2018-02-11 10:25:05 +0000

    Added more pages for demo

--- webserver.py
+++ webserver.py
@@ -3,17 +3,33 @@ from quickstart import create_event
 
 app = Flask(__name__)
 
-@app.route('/welcome')
+@app.route('/login')
 def welcome():
-    return app.send_static_file('welcome.html')
+    return app.send_static_file('login.html')
+
+@app.route('/setup')
+def setup():
+    return app.send_static_file('setup.html')
+
+@app.route('/success')
+def success():
+    return app.send_static_file('success.html')
+
+@app.route('/home')
+def home():
+    return app.send_static_file('home.html')
+
+@app.route('/notification')
+def notification():
+    return app.send_static_file('notification.html')
 
 @app.route('/send-invite')
 def sendInvite():
     create_event()
-    return redirect(url_for('success'))
+    return redirect(url_for('success-1'))
 
-@app.route('/success')
-def success():
+@app.route('/success-1')
+def success-1():
     return 'this works!'
 
 app.run(host="0.0.0.0")
